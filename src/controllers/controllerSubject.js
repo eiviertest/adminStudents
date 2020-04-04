4fcfa422fe31145dbff77f2cb8ce75629ce8e983
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2020-04-04 11:24:03 -0600

    Conexion MongoAtlas, CRUD Funcional Subject

--- src/controllers/controllerSubject.js
+++ src/controllers/controllerSubject.js
@@ -7,8 +7,8 @@ const Career = require("../models/Career");
 
 //Render view to show all subjects
 ctrlSubject.listSubjects = async (req, res) => {
-  const subject = await Subject.find().lean();
-  res.render("subject/listSubjects", { subject });
+  const subjects = await Subject.find().lean();
+  res.render("subject/listSubjects", { subjects });
 };
 
 //Render form to add a subject
@@ -21,34 +21,39 @@ ctrlSubject.addSubject = async (req, res) => {
 //Save a subject in DB
 ctrlSubject.saveSubject = async (req, res) => {
   //Destructuring from request
-  const { name } = req.body;
+  const { name, grade, careerKey } = req.body;
+  console.log(req.body);
   //Create the object with module mongoose
   const saveSubjectDB = new Subject({
     name,
+    grade,
+    careerKey,
   });
+  console.log(saveSubjectDB);
   //Save object created
   await saveSubjectDB.save();
-  res.redirect("/subject");
+  res.redirect("/subjects");
 };
 
 //Render form to update a career
 ctrlSubject.updateSubject = async (req, res) => {
   //Find a subject in DB with ID from request
   const subject = await Subject.findById(req.params.id).lean();
-  res.render("subject/updateSubject", { subject });
+  const careers = await Career.find().lean();
+  res.render("subject/updateSubject", { subject, careers });
 };
 
 //Update a career
 ctrlSubject.updateSubjectDB = async (req, res) => {
-  const { name } = req.body;
-  await Subject.findByIdAndUpdate(req.params.id, { name });
-  res.redirect("/subject");
+  const { name, grade, careerKey } = req.body;
+  await Subject.findByIdAndUpdate(req.params.id, { name, grade, careerKey });
+  res.redirect("/subjects");
 };
 
 //Delete a career
 ctrlSubject.deleteSubject = async (req, res) => {
   await Subject.findByIdAndDelete(req.params.id);
-  res.redirect("/subject");
+  res.redirect("/subjects");
 };
 
 module.exports = ctrlSubject;
--- src/database.js
+++ src/database.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
+const MONGODB_URI = `mongodb+srv://kapxyale:alex45eivier@herramientas-tdenp.mongodb.net/test?retryWrites=true&w=majority`;
 
-const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
-
-mongoose.connect(MONGODB_URI, {
+mongoose
+  .connect(MONGODB_URI, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   })
-  .then(db => console.log("Database is connected"))
-  .catch(err => console.log(err));
+  .then((db) => console.log("Database is connected"))
+  .catch((err) => console.log(err));
